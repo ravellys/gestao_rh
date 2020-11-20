c46f6302aeb44d47b0f41003462e34664da9a523
ravellys <lucas.ravellys@ufpe.br>
2020-11-20 10:47:13 -0300

    criada a app empresas

---
+++ apps/empresas/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+# Register your models here.
+from apps.empresas.models import Empresa
+
+admin.site.register(Empresa)
--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -16,7 +16,6 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
@@ -38,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.empresas',
 ]
 
 MIDDLEWARE = [
